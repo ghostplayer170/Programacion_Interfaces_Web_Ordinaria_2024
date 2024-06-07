399e94e2235f377f8777b45b6e66a52b0fc54f19
ghostplayer170 <82672414+ghostplayer170@users.noreply.github.com>
2024-06-07 13:38:58 +0200

    first-commit

--- routes/_layout.tsx
+++ routes/_layout.tsx
@@ -4,7 +4,7 @@ import Head from "../components/Head.tsx";
 // deno-lint-ignore require-await
 export default async function Layout(_req: Request, ctx: FreshContext) {
   return (
-    <div>
+    <div class="page-container">
       <Head userName={`${ctx.state.name || "Unknown"} `} />
       <ctx.Component />
     </div>
