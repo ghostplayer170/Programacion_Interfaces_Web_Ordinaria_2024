c9d1aa7e4ce11abdbb3316ca80453d7a43f735a5
ghostplayer170 <82672414+ghostplayer170@users.noreply.github.com>
2024-06-07 13:01:34 +0200

    first-commit

--- components/Videolist.tsx
+++ components/Videolist.tsx
@@ -1,9 +1,40 @@
 import { FunctionComponent } from "preact";
+import { video } from "../types.ts";
 
-const Videolist: FunctionComponent = () => {
+type Data = {
+  videos: video[];
+  userid: string;
+};
+
+const Videolist: FunctionComponent<Data> = ({ videos, userid }) => {
   return (
     <>
-      <h1>videos</h1>
+      <div class="video-page-container">
+        <h1 class="video-list-title">Curso Deno Fresh</h1>
+        <div class="video-list-container">
+          {videos.map((vid) => {
+            return (
+              <div class="video-item" key={vid.id}>
+                <a href={`/video/${vid.id}}`} class="video-link">
+                  <img
+                    src="https://i.ytimg.com/vi/0GuSxDagyVk/hqdefault.jpg"
+                    alt={vid.title}
+                    class="video-thumbnail"
+                  />
+                  <div class="video-info">
+                    <h3 class="video-title">{vid.title}</h3>
+                    <p class="video-description">{vid.description}</p>
+                    <p class="video-release-date">
+                      Release date: {new Date(vid.date).toLocaleDateString()}
+                    </p>
+                  </div>
+                </a>
+                <button class="fav-button">🤍 Add to Favorites</button>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 };
--- routes/videos.tsx
+++ routes/videos.tsx
@@ -1,14 +1,46 @@
-import { Handlers } from "$fresh/server.ts";
+import { FreshContext, Handlers, PageProps } from "$fresh/server.ts";
 import Videolist from "../components/Videolist.tsx";
+import { video } from "../types.ts";
 
-export const handler: Handlers = {
-  async GET(_req, ctx) {
-    return await ctx.render();
+type Data = {
+  videos: video[];
+  userid: string;
+};
+
+type State = {
+  id: string;
+  name: string;
+  email: string;
+};
+
+export const handler: Handlers<Data, State> = {
+  async GET(_req: Request, ctx: FreshContext<State, Data>) {
+    try {
+      const userid = ctx.state.id;
+      const API_URL = Deno.env.get("API_URL");
+      const response = await fetch(`${API_URL}/videos/${userid}`);
+      if (response.status === 200) {
+        const videos = await response.json();
+        return await ctx.render({ videos, userid });
+      }
+      if (response.status === 500) {
+        return new Response("Unexpected Error", { status: 500 });
+      }
+      if (response.status === 404) {
+        return new Response("User with id not found", { status: 404 });
+      } else {
+        return new Response("Internal Server Error: Fetching Videos", {
+          status: 500,
+        });
+      }
+    } catch (error) {
+      return new Response("Internal Server Error" + error, { status: 500 });
+    }
   },
 };
 
-const videoPage = () => {
-  return <Videolist />;
+const videoPage = (props: PageProps<Data>) => {
+  return <Videolist userid={props.data.userid} videos={props.data.videos} />;
 };
 
 export default videoPage;
